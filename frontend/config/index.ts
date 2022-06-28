c6d475cd0cb766e0b9117af8e86870ce8b6f0337
hwrdtm <howard.tam.95@gmail.com>
2022-06-28 00:33:20 -0700

    fix config

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -13,7 +13,7 @@ const config = {
   ),
   environment: process.env.ENVIRONMENT || "local",
   browserEnvironment: process.env.NEXT_PUBLIC_ENVIRONMENT || "local",
-  redisUrl: process.env.REDIS_URL || "http://localhost:6379",
+  redisUrl: process.env.NEXT_PUBLIC_REDIS_URL || "http://localhost:6379",
 };
 
 export const getContractAddress = () => {
