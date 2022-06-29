703a7193ef4deed1d311c0b12158b313be67fb44
hwrdtm <howard.tam.95@gmail.com>
2022-06-29 16:46:13 -0700

    try fix wasm ref bug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,29 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          global: "global",
-        })
-      );
-
-      config.resolve.fallback = {
-        fs: false,
-        stream: false,
-        crypto: false,
-        os: false,
-        readline: false,
-        ejs: false,
-        assert: require.resolve("assert"),
-        path: false,
-      };
-
-      return config;
+  // StrictMode render twice... https://github.com/vercel/next.js/issues/35822
+  reactStrictMode: false,
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false;
     }
-
     return config;
   },
   serverRuntimeConfig: {
